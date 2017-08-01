e7cacfb7b840d0b5f7cab6a2bbe28f385e25d168
saraharless <sara.e.harless@gmail.com>
2017-08-01 12:38:57 -0500

    initial

---
+++ main.js
@@ -0,0 +1,97 @@
+// Since the `data.js` file is loaded in your `index.html` before this one,
+// you have access to the `data` variable that contains the Etsy Store data.
+// Open your `index.html` page in your browser and you can verify that the following
+// is properly working. The `data` variable is an array with 25 items in it
+console.log(data);
+
+
+
+// 1: Show me how to calculate the average price of all items.
+function question1 () {
+  let total = 0;
+  for (var i = 0; i < data.length; i++) {
+    total += data[i].price
+    // console.log('TOTAL' , total);
+  }
+  let averagePrice = total / data.length;
+  console.log('AVERAGE' , averagePrice);
+  return averagePrice;
+
+  // Answer:
+}
+
+
+
+// 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
+function question2 () {
+  let newArray = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].price >= 14 && data[i].price <= 18)
+    newArray.push(data[i].price);
+  }
+  console.log(newArray);
+  return newArray;
+  // Answer:
+}
+
+
+
+// 3: Which item has a "GBP" currency code? Display it's name and price.
+function question3 () {
+  // Answer:
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].currency_code === 'GBP'){
+      console.log(data[i].currency_code);
+      return data[i].currency_code;
+    }
+
+  }
+}
+
+
+
+// 4: Display a list of all items who are made of wood.
+function question4 () {
+  // Answer:
+  let itemSelected = []
+  for (let i = 0; i < data.length; i++) {
+    for (let a = 0; a < data[i].materials.length; a++) {
+      if (data[i].materials[a] === 'wood'){
+        itemSelected.push(data[i])
+      }
+    }
+  }
+  for (let b = 0; b < itemSelected.length; b++) {
+    console.log(itemSelected[b].title);
+  }
+}
+
+
+// 5: Which items are made of eight or more materials?
+//    Display the name, number of items and the items it is made of.
+function question5 () {
+  // Answer:
+  let itemSelected = []
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].materials.length >= 8) {
+      itemSelected.push(data[i])
+    }
+  }
+  for (let i = 0; i < itemSelected.length; i++) {
+    console.log(itemSelected[i].title);
+  }
+}
+
+
+// 6: How many items were made by their sellers?
+// Answer:
+function question6 () {
+  let itemSelected = []
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].who_made === 'i_did') {
+      itemSelected.push(data[i])
+    }
+  }
+  console.log(itemSelected.length);
+  // Answer:
+}
